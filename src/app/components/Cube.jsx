9d19337204c6e0848c7ef9967a2bdbbc0d9241ef
pouriya moshfegh <pouriya.moshfeghs@gmail.com>
2023-07-09 22:00:34 +0330

    finished

---
+++ src/app/components/Cube.jsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { useRef } from "react";
+import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+const Cube = () => {
+  const textRef = useRef();
+  useFrame(
+    (state) =>
+      (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2)
+  );
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshStandardMaterial>
+        <RenderTexture attach="map">
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+          <color attach="background" args={["#dc9dcd"]} />
+          <Text ref={textRef} fontSize={1} color="#555">
+           pouriya Moshfegh
+          </Text>
+        </RenderTexture>
+      </meshStandardMaterial>
+    </mesh>
+  );
+};
+
+export default Cube;
